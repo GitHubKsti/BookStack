ac1b18ee6b38c8296ef51f0d7958a065924560a9
Timo Heinzelmann <timo.heinzelmann@ksti.de>
2022-08-12 09:08:47 +0200

    only support renaming of page links

--- app/Entities/Models/Page.php
+++ app/Entities/Models/Page.php
@@ -198,19 +198,18 @@ class Page extends BookChild
             if(preg_match_all("/$regexp/siU", $html, $matches)) {
                 foreach($matches[2] as $match)
                 {
-                    $url = explode('/', $match);
-                    $lastPart = array_pop($url);
-                    foreach($tempPages->keys() as $key)
-                    {
-                        $subPage = $tempPages[$key];
-                        if($subPage->slug == $lastPart)
-                        {
-                            $replaceString = "#page-" . $subPage->id;
-                            if($replacedValues)
-                            {
-                                $replacedValues[$match] = $replaceString;
+                    if(str_contains($match, "/pages/") or str_contains($match, "/page/")) {
+                        $url = explode('/', $match);
+                        $lastPart = array_pop($url);
+                        foreach ($tempPages->keys() as $key) {
+                            $subPage = $tempPages[$key];
+                            if ($subPage->slug == $lastPart) {
+                                $replaceString = "#page-" . $subPage->id;
+                                if ($replacedValues) {
+                                    $replacedValues[$match] = $replaceString;
+                                }
+                                $page->html = str_replace($match, $replaceString, $page->html);
                             }
-                            $page->html = str_replace($match, $replaceString, $page->html);
                         }
                     }
                 }
